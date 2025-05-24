a537322b2a4eb4df012ec887e23a04f8f6899436
word-user <work@sys.com>
2025-05-24 08:59:15 +0300

    init project and project theme

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import Swiper from "swiper";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'aos/dist/aos.js';
+import '@srexi/purecounterjs/dist/purecounter_vanilla.js';
+import 'glightbox/dist/js/glightbox.min.js';
+import './main.js';
---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blogy - {{$title ?? 'Home'}}</title>
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    @vite("resources/css/app.css")
+</head>
+<body>
+    {{$slot}}
+
+    @vite('resources/js/app.js')
+</body>
+</html>
